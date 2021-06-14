25960def52a7e1a44056f105a52c41d9945d66ef
swapno <swapno5896@yahoo.com>
2021-06-14 04:48:14 -0700

    structer done

---
+++ src/components/Appointment/AppontmentForm/AppontmentForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppontmentForm = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AppontmentForm;
---
+++ src/components/Appointment/MinAppoinment/Appointment.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopBanner from '../TopBanner/TopBanner';
+import AppontmentForm from './../AppontmentForm/AppontmentForm';
+
+const Appointment = () => {
+    return (
+        <div>
+            <TopBanner />
+            <AppontmentForm />
+        </div>
+    );
+};
+
+export default Appointment;
