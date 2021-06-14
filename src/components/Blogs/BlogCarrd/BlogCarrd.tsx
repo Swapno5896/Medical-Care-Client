77d2505a72c8073a88f0c7a707b156e3a2cbfa2e
swapno <swapno5896@yahoo.com>
2021-06-14 04:14:52 -0700

    detail blog

--- src/components/Blogs/BlogCarrd/BlogCarrd.tsx
+++ src/components/Blogs/BlogCarrd/BlogCarrd.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-
+import img1 from '../../../images/c-1.jpg'
 const BlogCarrd = () => {
     return (
         <div>
 
+            <div className="col">
+                <div className="card">
+                    <img src={img1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src={img1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src={img1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src={img1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/components/Blogs/MainBlog/MainBlog.tsx
+++ src/components/Blogs/MainBlog/MainBlog.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import BlogCarrd from './../BlogCarrd/BlogCarrd';
 
 const MainBlog = () => {
     return (
         <div>
-
+            <div className="row row-cols-1 row-cols-md-2 g-4">
+                <BlogCarrd />
+            </div>
         </div>
     );
 };
