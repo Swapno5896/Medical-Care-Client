1381beb4b5c05c12f099b71b0d7445f6e457e375
swapno <swapno5896@yahoo.com>
2021-06-14 04:25:10 -0700

    blog section is done

---
+++ src/components/Blogs/BlogComments/BlogComments.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const BlogComments = () => {
+    return (
+        <div>
+
+            <div className="row">
+                <h2>Add comment</h2>
+                <div className="col-md-4"> <input type="text" /></div>
+                <div className="col-md-4"> <input type="text" /></div>
+                <div className="col-md-4"> <input type="text" /></div>
+                <div className="col-md-12"> <input type="text" /></div>
+            </div>
+        </div>
+    );
+};
+
+export default BlogComments;
--- src/components/Blogs/SingleBlog/SingleBlog.tsx
+++ src/components/Blogs/SingleBlog/SingleBlog.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import BlogComments from '../BlogComments/BlogComments';
+import DetailBlog from '../DetainBlog/DetainBlog'
+import SideBlogs from '../SideBlogs/SideBlogs';
 const SingleBlog = () => {
     return (
         <div>
-
+            <div className="row">
+                <div className="col-md-8">
+                    <DetailBlog />
+                    <BlogComments></BlogComments>
+                </div>
+                <div className="col-md-4">
+                    <SideBlogs></SideBlogs>
+                </div>
+            </div>
         </div>
     );
 };
