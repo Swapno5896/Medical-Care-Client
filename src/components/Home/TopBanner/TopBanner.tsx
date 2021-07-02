55eb656486e45c2010bbe02ed6eecfe5e54cb014
swapno <swapno5896@yahoo.com>
2021-07-02 13:41:44 -0700

    carosel is updated

--- src/components/Home/TopBanner/TopBanner.tsx
+++ src/components/Home/TopBanner/TopBanner.tsx
@@ -9,7 +9,7 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const TopBanner = () => {
     return (
-        <div>
+        <div style={{ marginTop: '85px' }}>
             <div id="carouselExampleDark" className="top-banner-carosel carousel carousel-dark slide" data-bs-ride="carousel">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -17,48 +17,71 @@ const TopBanner = () => {
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
                 <div className="carousel-inner">
-                    <div className="carousel-item active" data-bs-interval="10000">
+                    <div className="carousel-item active" data-bs-interval="1000">
                         <div className="row">
                             <div className="col-md-6">
-                                <h1>Provide best <br /> medical care</h1>
-                                <p>The health and well-being of our patients and their health care team will
-                                    always be our priority, so we follow the best practices for cleanliness.</p>
-                            </div>
-                            <div className="col-md-6">
-                                <img src={img1} className="d-block w-100" alt="..." />
-                            </div>
-                        </div>
 
-
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h1>Provide best <br /> medical care</h1>
-                                <p>The health and well-being of our patients and their health care team will
-                                    always be our priority, so we follow the best practices for cleanliness.</p>
                                 <div className='top-banner-icons'>
                                     <p> <span><FontAwesomeIcon icon={faCoffee} /></span> Examination</p>
                                     <p><span><FontAwesomeIcon icon={faCoffee} /></span>Prescription</p>
                                     <p><span><FontAwesomeIcon icon={faCoffee} /></span>Cardiogram</p>
                                     <p><span><FontAwesomeIcon icon={faCoffee} /></span>Blood Pressure</p>
+                                    <div>
+                                        <h1>Provide best <br /> firsat care care</h1>
+                                        <p>The health and well-being of our patients and their health care team will
+                                            always be our priority, so we follow the best practices for cleanliness.</p>
+                                    </div>
 
                                 </div>
                             </div>
                             <div className="col-md-6">
-                                <img src={img1} className="d-block w-100" alt="..." />
+                                <img src={img2} className="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className="carousel-item" data-bs-interval="1000">
+                        <div className="row">
+                            <div className="col-md-6">
+
+                                <div className='top-banner-icons'>
+                                    <p> <span><FontAwesomeIcon icon={faCoffee} /></span> Examination</p>
+                                    <p><span><FontAwesomeIcon icon={faCoffee} /></span>Prescription</p>
+                                    <p><span><FontAwesomeIcon icon={faCoffee} /></span>Cardiogram</p>
+                                    <p><span><FontAwesomeIcon icon={faCoffee} /></span>Blood Pressure</p>
+                                    <div>
+                                        <h1>Provide best <br /> swapno care care</h1>
+                                        <p>The health and well-being of our patients and their health care team will
+                                            always be our priority, so we follow the best practices for cleanliness.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <img src={img2} className="d-block w-100" alt="..." />
                             </div>
                         </div>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="1000">
                         <div className="row">
                             <div className="col-md-6">
-                                <h1>Provide best <br /> medical care</h1>
-                                <p>The health and well-being of our patients and their health care team will
-                                    always be our priority, so we follow the best practices for cleanliness.</p>
+
+                                <div className='top-banner-icons'>
+                                    <p> <span><FontAwesomeIcon icon={faCoffee} /></span> Examination</p>
+                                    <p><span><FontAwesomeIcon icon={faCoffee} /></span>Prescription</p>
+                                    <p><span><FontAwesomeIcon icon={faCoffee} /></span>Cardiogram</p>
+                                    <p><span><FontAwesomeIcon icon={faCoffee} /></span>Blood Pressure</p>
+                                    <div>
+                                        <h1>Provide best <br /> medical care</h1>
+                                        <p>The health and well-being of our patients and their health care team will
+                                            always be our priority, so we follow the best practices for cleanliness.</p>
+                                    </div>
+
+                                </div>
                             </div>
                             <div className="col-md-6">
-                                <img src={img1} className="d-block w-100" alt="..." />
+                                <img src={img2} className="d-block w-100" alt="..." />
                             </div>
                         </div>
                     </div>
